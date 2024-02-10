3764a6345ea0ae88b64eb5875a0c36a7ad34b9cd
NahGoKrazy <mukatinabeel1@gmail.com>
2024-02-10 15:23:26 -0500

    ft

--- Orange Health/Orange Health/ContentView.swift
+++ Orange Health/Orange Health/ContentView.swift
@@ -23,3 +23,25 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+
+struct Exercise {
+    var ex_name: String // string
+    var targ_musc: String // string
+    var sets: Int // int
+    var reps: Int // int
+    var weight: Int // int
+    var goal: Int // int
+    var rest_time: Int // seconds
+
+    init(ex_name: String, targ_musc: String, sets: int, reps: Int, weight: Int, goal: Int, rest_time: Int) {
+        self.ex_name = ex_name;
+        self.targ_musc = targ_musc;
+        self.sets = sets;
+        self.reps = reps;
+        self.weight = weight;
+        self.goal = goal;
+        self.rest_time = rest_time;
+    }
+}
