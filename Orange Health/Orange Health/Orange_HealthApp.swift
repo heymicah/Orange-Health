d9033fec7e57f57b892e431bd6ff7c46b1adb0eb
Micah <86578095+heymicah@users.noreply.github.com>
2024-02-10 14:22:59 -0500

    Add files via upload

---
+++ Orange Health/Orange Health/Orange_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Orange_HealthApp.swift
+//  Orange Health
+//
+//  Created by Micah Tam on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Orange_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
